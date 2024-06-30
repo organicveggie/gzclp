709037a733ceb51de401965bf44dc733a0a3dcfa
Sean Laurent <organicveggie@gmail.com>
2024-06-29 19:54:55 -0700

    Update LocalStorageSettingsApi with json serialization
    Also adds matching tests.

--- gzclp_flutter/packages/local_storage_settings_api/lib/src/local_storage_settings_api.dart
+++ gzclp_flutter/packages/local_storage_settings_api/lib/src/local_storage_settings_api.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:rxdart/subjects.dart';
 import 'package:settings_api/settings_api.dart';
@@ -25,7 +26,8 @@ class LocalStorageSettingsApi extends SettingsApi {
   void _init() {
     final appSettingsJson = _getValue(kAppSettingsKey);
     if (appSettingsJson != null) {
-      final settings = _settingsApiSerializer.appSettingsFromJson(appSettingsJson);
+      final json = jsonDecode(appSettingsJson);
+      final settings = _settingsApiSerializer.appSettingsFromJson(json);
       _settingsStreamController.add(settings ?? AppSettings.withDefaults());
     } else {
       _settingsStreamController.add(AppSettings.withDefaults());
@@ -39,9 +41,9 @@ class LocalStorageSettingsApi extends SettingsApi {
   Future<void> saveAppSettings(AppSettings settings) {
     final appSettingsJson = _settingsApiSerializer.appSettingsToJson(settings);
     if (appSettingsJson != null) {
-      return _setValue(kAppSettingsKey, appSettingsJson.toString());
+      return _setValue(kAppSettingsKey, appSettingsJson);
     }
 
-    throw const FormatException('unable save app settings');
+    throw const FormatException('unable serialize app settings');
   }
 }
--- gzclp_flutter/packages/local_storage_settings_api/test/local_storage_settings_api_test.dart
+++ gzclp_flutter/packages/local_storage_settings_api/test/local_storage_settings_api_test.dart
@@ -1,12 +1,53 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:convert';
 
+import 'package:flutter_test/flutter_test.dart';
 import 'package:local_storage_settings_api/local_storage_settings_api.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:settings_api/settings_api.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class MockSharedPreferences extends Mock implements SharedPreferences {}
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group(LocalStorageSettingsApi, () {
+    const rawJson = '''{"gender": "xx", "units": "kg", "microplates": true, "barWeight": "kg15"}''';
+    final appSettings = AppSettings((b) => b
+      ..gender = Gender.xx
+      ..units = WeightUnit.kg
+      ..microplates = true
+      ..barWeight = BarWeight.kg15);
+
+    late SharedPreferences plugin;
+
+    setUp(() {
+      plugin = MockSharedPreferences();
+      when(() => plugin.getString(any())).thenReturn(rawJson);
+      when(() => plugin.setString(any(), any())).thenAnswer((_) async => true);
+    });
+
+    LocalStorageSettingsApi createSubject() {
+      return LocalStorageSettingsApi(plugin: plugin);
+    }
+
+    group('constructor', () {
+      test('works correctly', () {
+        expect(createSubject, returnsNormally);
+      });
+
+      group('initializes the stream', () {
+        test('with existing app settings', () {
+          final subject = createSubject();
+          expect(subject.getAppSettings(), emits(appSettings));
+          verify(() => plugin.getString(LocalStorageSettingsApi.kAppSettingsKey)).called(1);
+        });
+
+        test('with default when no settings', () {
+          when(() => plugin.getString(any())).thenReturn(null);
+          final subject = createSubject();
+          expect(subject.getAppSettings(), emits(AppSettings.withDefaults()));
+          verify(() => plugin.getString(LocalStorageSettingsApi.kAppSettingsKey)).called(1);
+        });
+      });
+    });
   });
 }
